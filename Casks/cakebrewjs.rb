729ec0ba6fb598156d3d595f1e87cf3e5b3b1459
osx user <gili.shemesh@gmail.com>
2020-12-14 21:27:20 +0200

    commit

--- Casks/cakebrewjs.rb
+++ Casks/cakebrewjs.rb
@@ -1,6 +1,6 @@
 cask "cakebrewjs" do
-  version "0.1.8"
-  sha256 "5122488d43ee41a73ea9cca1ecf6a0a80f40a328ba629b74a20ba3c56192b5b1"
+  version "0.1.9"
+  sha256 "0cf4834ec710412431850bd81223065add69ccc41057a4c34d431cca625912b1"
 
   url "https://downloads.sourceforge.net/cakebrewjs/v#{version}/cakebrewjs-#{version}-mac.zip"
   appcast "https://sourceforge.net/projects/cakebrewjs/rss?"
