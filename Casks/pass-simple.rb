0c9815da03efe1705ff899c7dd7c96d193164b97
osx user <gili.shemesh@gmail.com>
2023-02-06 17:17:26 +0200

    commit

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -2,7 +2,7 @@ cask "pass-simple" do
   version "0.4.0"
   sha256 "11cf053f79f1bc44525eb9751b99569ac14ab0a96fe5a6300fe425e9f7764333"
 
-  url "https://downloads.sourceforge.net/pass-simple/pass-simple-osx_0.3.0.dmg"
+  url "https://downloads.sourceforge.net/pass-simple/pass-simple-osx_0.4.0.dmg"
   appcast "https://sourceforge.net/projects/pass-simple/rss?"
   name "Pass simple"
   desc "GUI for pass cli util"
