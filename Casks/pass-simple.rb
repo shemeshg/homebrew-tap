b0bb3465a3f260f78cf4a4429e754f3c806740fb
shemeshg <gili.shemesh@gmail.com>
2023-07-13 21:23:58 +0300

    Update pass-simple.rb

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -3,11 +3,16 @@ cask "pass-simple" do
   sha256 "225f443ebb1e24da40b1d17ef826ff5fb97b84e51176c20067d41e85535e67c1"
 
   url "https://downloads.sourceforge.net/pass-simple/pass-simple-macos_#{version}.zip"
-  appcast "https://sourceforge.net/projects/pass-simple/rss?"
   name "Pass simple"
   desc "GUI for pass cli util"
   homepage "https://sourceforge.net/projects/pass-simple/"
 
+  livecheck do
+    url "https://sourceforge.net/projects/pass-simple/rss?"
+    regex(/pass-simple-macos-(\d+(?:\.\d+)+)\.zip/i)
+    strategy :page_match
+  end
+  
   app "Pass simple.app"
 
 
