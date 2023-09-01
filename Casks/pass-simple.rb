64cbe8f6327b3d5c9ffd9bdbc912dbb4d0d93bca
shemeshg <gili.shemesh@gmail.com>
2023-09-01 16:45:10 +0300

    commit

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,6 +1,6 @@
 cask "pass-simple" do
-  version "0.86.0"
-  sha256 "7a7504240078e523f5fac3dd814563ccd71e086c4453f1df7f37ddd4e016a0c4"
+  version "0.87.0"
+  sha256 "6a0a3aae36bc595c752f956baaa9e4a149b12bc048f165353f32541aba424b07"
 
   url "https://downloads.sourceforge.net/pass-simple/pass-simple-macos_#{version}.zip"
   name "Pass simple"
