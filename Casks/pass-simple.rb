8f3b4e3ec7cd2d3e7e3fa7861ecf4edf9869dee2
shemeshg <gili.shemesh@gmail.com>
2023-10-18 03:00:29 +0300

    commit

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -2,7 +2,7 @@ cask "pass-simple" do
   version "0.108"
   sha256 "68b0fde71de511e74db90911475d0f1b065c5392465470482f3971bd0a6cc200"
 
-  url "https://downloads.sourceforge.net/pass-simple/pass-simple-macos_#{version}.zip"
+  url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-Darwin.dmg"
   name "Pass simple"
   desc "GUI for pass cli util"
   homepage "https://sourceforge.net/projects/pass-simple/"
