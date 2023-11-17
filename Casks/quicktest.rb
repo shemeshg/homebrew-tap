161a03c27f535767a78346a65432989f66aaaec3
shemeshg <gili.shemesh@gmail.com>
2023-11-17 19:15:44 +0200

    commit

--- Casks/quicktest.rb
+++ Casks/quicktest.rb
@@ -3,7 +3,7 @@ cask "quicktest" do
   sha256 "a02b2a1dda7f877d2b8f49298db6325b1acd237a21f97e92b8e9c73dd66e97c7"
 
   url "https://downloads.sourceforge.net/pass-simple/quicktest.dmg"
-  name "Pass simple"
+  name "quicktest"
   desc "GUI for pass cli util"
   homepage "https://sourceforge.net/projects/pass-simple/"
 
