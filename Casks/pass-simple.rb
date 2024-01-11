2f1ac0009d56db76f6fa36e6c69e5fc6a502e104
shemeshg <gili.shemesh@gmail.com>
2024-01-11 12:42:25 +0200

    0.153

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,6 +1,6 @@
 cask "pass-simple" do
-  version "0.151"
-  sha256 "594b5bf233311a1eafdffe8bfb084c3c9da2925703d27638ff617b2018886292"
+  version "0.153"
+  sha256 "ced404c429d90a322f73d2e2cf43c16850bd4e3647679bb604cfca0c1c09eb41"
 
   url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-Darwin.dmg"
   name "Pass simple"
