f73eea19d51e8d2c92d84e06abc3476cbc526c8c
shemeshg <gili.shemesh@gmail.com>
2024-10-10 19:28:24 +0300

    commit

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -2,7 +2,7 @@ cask "pass-simple" do
   version "1.0.15"
   sha256 "ed8ee08bc5c0b3a5073607ae1eb85575ac07a431cca9281166b8b67fec945fda"
 
-  url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-Darwin.dmg"
+  url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-x86_64.dmg"
   name "Pass simple"
   desc "GUI for pass cli util"
   homepage "https://sourceforge.net/projects/pass-simple/"
