74a6c892f2cc2b9eced4091b7029f07a21952992
macos user <macos@std1013.Dlink>
2024-10-14 07:24:03 -0700

    1.0.15

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,5 +1,5 @@
 cask "pass-simple" do
-  arch arm: "arm64", intel: "x86_64"
+  arch arm: "4f4166bec89ee902c8d79e2d6cd5028944615656c3afe0b661b2d55c9389cc81"
   version "1.0.15"
   sha256  arm: "66259a7c020c60a6e842ed8a263b1aea0c5b43c894497d03576266fda9c1f577",
           intel: "ed8ee08bc5c0b3a5073607ae1eb85575ac07a431cca9281166b8b67fec945fda"
