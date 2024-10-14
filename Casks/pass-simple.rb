33e90fec1c8ee4c9fd4c31545848a28f7b3a3522
shemeshg <gili.shemesh@gmail.com>
2024-10-14 04:00:51 +0300

    added arch64

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,8 +1,10 @@
 cask "pass-simple" do
+  arch arm: "arm64", intel: "x86_64"
   version "1.0.15"
-  sha256 "ed8ee08bc5c0b3a5073607ae1eb85575ac07a431cca9281166b8b67fec945fda"
+  sha256  arm: "66259a7c020c60a6e842ed8a263b1aea0c5b43c894497d03576266fda9c1f577",
+          intel: "ed8ee08bc5c0b3a5073607ae1eb85575ac07a431cca9281166b8b67fec945fda"
 
-  url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-x86_64.dmg"
+  url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-#{arch}.dmg"
   name "Pass simple"
   desc "GUI for pass cli util"
   homepage "https://sourceforge.net/projects/pass-simple/"
