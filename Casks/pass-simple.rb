3d7fa0b4e5c9220e428ccf3213c54df292294878
shemeshg <gili.shemesh@gmail.com>
2025-02-06 08:20:07 +0200

    commit

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,7 +1,7 @@
 cask "pass-simple" do
   arch arm: "arm64", intel: "x86_64"
   version "1.0.19"
-  sha256  arm: "efc8c61182e380acae3a5461814df41eeb6ad590025fe09fc356bc6ad5bf2565"
+  sha256  arm: "574fc9298ab937905e3623f5a1960598cf879f890a8e09d0b431cfadf7d1a714"
           
 
   url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-#{arch}.dmg"
@@ -15,7 +15,7 @@ cask "pass-simple" do
   depends_on formula: "botan"
   depends_on formula: "json-c"  
 
-  app "pass simple.app"
+  app "pass-simple.app"
 
   zap trash: [
     "~/Library/Caches/Pass simple",
