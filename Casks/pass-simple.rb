482aa648295e689a690be9e151e1787a04d8869e
shemeshg <gili.shemesh@gmail.com>
2025-03-01 16:55:43 +0200

    commit

--- Casks/pass-simple.rb
+++ Casks/pass-simple.rb
@@ -1,7 +1,7 @@
 cask "pass-simple" do
   arch arm: "arm64", intel: "x86_64"
-  version "1.0.19"
-  sha256  arm: "cd2505b7ebb741b12ace3e018880c38cb1699ea77e569635395f03bff753da8d"
+  version "1.0.20"
+  sha256  arm: "46a1eccc142e3a1dc858e3a53871ea37cb81c911d360b84c158420f29d206cc4"
           
 
   url "https://downloads.sourceforge.net/pass-simple/pass-simple-#{version}-#{arch}.dmg"
