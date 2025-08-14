40d6d3dbc135f68a00d0e08fcdcf6e25d227912d
shemeshg <gili.shemesh@gmail.com>
2025-08-14 22:09:26 +0300

    commit

---
+++ Casks/cakebrewjs.rb
@@ -0,0 +1,26 @@
+cask "cakebrewjs" do
+  version "2.81"
+  sha256 "b7d724f0041653f20d16459d3cd87d07c001ee2e181297538c8ac3f1fb993eef"
+
+  url "https://downloads.sourceforge.net/cakebrewjs/Cakebrewjs-#{version}-Darwin.dmg"
+  name "cakebrewjs"
+  desc "Homebrew GUI app"
+  homepage "https://sourceforge.net/projects/cakebrewjs/"
+
+  livecheck do
+    url :url
+    regex(%r{url=.*?/cakebrewjs[._-]v?(\d+(?:\.\d+)+)(?:[._-]Darwin)?\.dmg}i)
+  end
+
+  app "cakebrewjs.app"
+
+  zap trash: [
+    "~/Library/Application Support/cakebrewjs",
+    "~/Library/Caches/cakebrewjs",
+    "~/Library/Caches/CakebrewJs2App",
+    "~/Library/Caches/shemeshg/Cakebrewjs2",
+    "~/Library/Preferences/com.electron.cakebrewjs.helper.plist",
+    "~/Library/Preferences/com.electron.cakebrewjs.plist",
+    "~/Library/Preferences/com.shemeshg.Cakebrewjs2.plist",
+  ]
+end
